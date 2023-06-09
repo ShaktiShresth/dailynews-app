ef9495ca6c937f178805483619684bee61fff2b7
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2023-06-09 17:00:14 +0545

    change to function based component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,84 +1,75 @@
-import React, { Component } from "react";
 import { Link } from "react-router-dom";
-// import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  //   static propTypes = {};
-
-  render() {
-    return (
-      <div
-        className="position-sticky top-0 left-0 p-0 m-0"
-        style={{ zIndex: "999" }}
-      >
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">
-              DailyNews
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <Link className="nav-link" aria-current="page" to="/">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/business">
-                    Business
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/entertainment">
-                    Entertainment
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/general">
-                    General
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/health">
-                    Health
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/science">
-                    Science
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/sports">
-                    Sports
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/technology">
-                    Technology
-                  </Link>
-                </li>
-              </ul>
-            </div>
+const Navbar = () => {
+  return (
+    <div
+      className="position-sticky top-0 left-0 p-0 m-0"
+      style={{ zIndex: "999" }}
+    >
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            DailyNews
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/business">
+                  Business
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/entertainment">
+                  Entertainment
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/general">
+                  General
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/health">
+                  Health
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/science">
+                  Science
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sports">
+                  Sports
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/technology">
+                  Technology
+                </Link>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </div>
-    );
-  }
-}
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,49 +1,44 @@
-import React, { Component } from "react";
-
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } =
-      this.props;
-    return (
-      <div>
-        <div className="card">
-          <span
-            className="position-absolute top-0 translate-middle badge rounded-pill bg-danger z-1"
-            style={{ zIndex: "1", left: "50%" }}
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div>
+      <div className="card">
+        <span
+          className="position-absolute top-0 translate-middle badge rounded-pill bg-danger z-1"
+          style={{ zIndex: "1", left: "50%" }}
+        >
+          {source}
+        </span>
+        <img
+          src={
+            !imageUrl
+              ? "https://c.ndtvimg.com/2023-05/60ptnvr_virat-kohli-bcci_625x300_23_May_23.jpg?im=FeatureCrop,algorithm=dnn,width=806,height=605"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small>
+              By {author ? author : "Unknown"} on{" "}
+              {new Date(date).toDateString()}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-dark"
           >
-            {source}
-          </span>
-          <img
-            src={
-              !imageUrl
-                ? "https://c.ndtvimg.com/2023-05/60ptnvr_virat-kohli-bcci_625x300_23_May_23.jpg?im=FeatureCrop,algorithm=dnn,width=806,height=605"
-                : imageUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text">
-              <small>
-                By {author ? author : "Unknown"} on{" "}
-                {new Date(date).toDateString()}
-              </small>
-            </p>
-            <a
-              rel="noreferrer"
-              href={newsUrl}
-              target="_blank"
-              className="btn btn-sm btn-dark"
-            >
-              Read More
-            </a>
-          </div>
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
